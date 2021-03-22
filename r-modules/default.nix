e8ad7d857670e37644b73b93f145663ee59f18a5
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2021-03-22 21:55:37 +0100

    fixed some bioc packages, added newer cytolib package
    which otherwise would not build with bioc 3.12

--- r-modules/default.nix
+++ r-modules/default.nix
@@ -250,12 +250,14 @@ let
     ChemmineOB = [ pkgs.openbabel pkgs.pkg-config ];
     cit = [ pkgs.gsl_1 ];
     curl = [ pkgs.curl.dev ];
+    CytoML = [ pkgs.hdf5.dev pkgs.libxml2.dev ];
     data_table = [pkgs.zlib.dev] ++ lib.optional stdenv.isDarwin pkgs.llvmPackages.openmp;
     devEMF = [ pkgs.xorg.libXft.dev pkgs.x11 ];
     diversitree = [ pkgs.gsl_1 pkgs.fftw ];
     EMCluster = [ pkgs.lapack ];
     fftw = [ pkgs.fftw.dev ];
     fftwtools = [ pkgs.fftw.dev ];
+    flowWorkspace = [ pkgs.hdf5.dev ];
     Formula = [ pkgs.gmp ];
     gdtools = [ pkgs.cairo.dev pkgs.fontconfig.lib pkgs.freetype.dev ];
     git2r = [ pkgs.zlib.dev pkgs.openssl.dev pkgs.libssh2.dev pkgs.libgit2 pkgs.pkg-config ];
@@ -281,6 +283,7 @@ let
     mvabund = [ pkgs.gsl_1 ];
     mwaved = [ pkgs.fftw.dev ];
     ncdf4 = [ pkgs.netcdf ];
+    ncdfFlow = [ pkgs.hdf5.dev ];
     nloptr = [ pkgs.nlopt pkgs.pkg-config ];
     n1qn1 = [ pkgs.gfortran ];
     odbc = [ pkgs.unixODBC ];
@@ -309,7 +312,7 @@ let
     Rglpk = [ pkgs.glpk ];
     RGtk2 = [ pkgs.gtk2.dev ];
     rhdf5 = [ pkgs.zlib ];
-    Rhdf5lib = [ pkgs.zlib ];
+    Rhdf5lib = [ pkgs.zlib pkgs.zlib.dev pkgs.hdf5 pkgs.hdf5.dev ];
     Rhpc = [ pkgs.zlib pkgs.bzip2.dev pkgs.icu pkgs.lzma.dev pkgs.mpi pkgs.pcre.dev ];
     Rhtslib = [ pkgs.zlib.dev pkgs.automake pkgs.autoconf pkgs.bzip2.dev pkgs.lzma.dev pkgs.curl.dev ];
     rjags = [ pkgs.jags ];
@@ -666,6 +669,21 @@ let
         '';
     });
 
+# CHzS Begin
+
+    cytolib = buildRPackage rec {
+      name = "cytolib";
+      version = "2.3.2";
+      src = fetchurl {
+        urls = [ "https://github.com/RGLab/cytolib/archive/refs/tags/bioc_3.13.tar.gz" ];
+        sha256 = "lcGFr5o9j4PFekTaNmE86LAcwcxeFcVOv4rZ2HsgXD8=";
+      };
+      propagatedBuildInputs = with new; [ BH Rcpp RcppArmadillo RcppParallel Rhdf5lib RProtoBufLib ];
+      buildInputs = [ R pkgs.hdf5.dev ];
+    };
+
+# CHzS End
+
     xml2 = old.xml2.overrideDerivation (attrs: {
       preConfigure = ''
         export LIBXML_INCDIR=${pkgs.libxml2.dev}/include/libxml2
@@ -725,7 +743,7 @@ let
     });
 
     Rhdf5lib = old.Rhdf5lib.overrideDerivation (attrs: {
-      patches = [ ./patches/Rhdf5lib.patch ];
+      # patches = [ ./patches/Rhdf5lib.patch ];
     });
 
     rJava = old.rJava.overrideDerivation (attrs: {
